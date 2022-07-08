9d940d50bf35fe08354fbbb92388a36dda358113
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-07-08 08:53:32 -0600

    feat: Added PrintToConsole class

--- Assets/Scripts/PrintToConsole.cs
+++ Assets/Scripts/PrintToConsole.cs
@@ -7,7 +7,7 @@ public class PrintToConsole : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Whatever I want to have print out");
     }
 
     // Update is called once per frame
